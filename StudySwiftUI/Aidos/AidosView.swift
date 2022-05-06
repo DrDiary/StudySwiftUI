ae09e198eb4a648d98f4ac70c2e214d3632e8727
HanSJin <kksd9900@naver.com>
2022-05-06 13:56:32 +0900

    first commit

---
+++ StudySwiftUI/Aidos/AidosView.swift
@@ -0,0 +1,8 @@
+//
+//  AidosView.swift
+//  StudySwiftUI
+//
+//  Created by 한승진 on 2022/05/06.
+//
+
+import Foundation
---
+++ StudySwiftUI/HanSJin/HanSJinView.swift
@@ -0,0 +1,8 @@
+//
+//  HanSJinView.swift
+//  StudySwiftUI
+//
+//  Created by 한승진 on 2022/05/06.
+//
+
+import Foundation
