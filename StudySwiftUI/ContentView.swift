0ae10905420ecc543e7f8b796c624bcc961a70a3
HanSJin <kksd9900@naver.com>
2022-05-06 13:09:09 +0900

    Initial Commit

---
+++ StudySwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StudySwiftUI
+//
+//  Created by 한승진 on 2022/05/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StudySwiftUI/StudySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudySwiftUIApp.swift
+//  StudySwiftUI
+//
+//  Created by 한승진 on 2022/05/06.
+//
+
+import SwiftUI
+
+@main
+struct StudySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
